ee480e5ba28a20273d28353fea9e0b4a74c49120
Erwin <ewib4@student.monash.edu>
2018-06-03 15:36:18 +1000

    Changes in demo
    I still use the demo folder tor testing purposes.
    
    I will tidy it up once I am finished with my thesis

--- Rlgt/demo/M3LGT.R
+++ Rlgt/demo/M3LGT.R
@@ -15,7 +15,7 @@ for (iter in 172:400) {
   data.train <- M3.data[[iter]]$x
   data.test <- M3.data[[iter]]$xx
   rstanmodel <- fit.lgt(data.train, model="LGT", nCores=2, nChains=4,
-                          control=lgt.control(MAX_NUM_OF_REPEATS=10, NUM_OF_ITER=1000), 
+                          control=lgt.control(MAX_NUM_OF_REPEATS=3, NUM_OF_ITER=1000), 
                           verbose=FALSE)
   sizeTestSet <- length(data.test)
   forecast_lgt[[iter]] <- forecast(rstanmodel, h = sizeTestSet, level=c(80, 95, 98))$mean
--- Rlgt/demo/M3LGTpart2.R
+++ Rlgt/demo/M3LGTpart2.R
@@ -15,7 +15,7 @@ for (iter in 401:length(M3.data)) {
   data.train <- M3.data[[iter]]$x
   data.test <- M3.data[[iter]]$xx
   rstanmodel <- fit.lgt(data.train, model="LGT", nCores=1, nChains=4,
-                        control=lgt.control(MAX_NUM_OF_REPEATS=10, NUM_OF_ITER=1000), 
+                        control=lgt.control(MAX_NUM_OF_REPEATS=3, NUM_OF_ITER=1000), 
                         verbose=FALSE)
   sizeTestSet <- length(data.test)
   forecast_lgt[[iter]] <- forecast(rstanmodel, h = sizeTestSet, level=c(80, 95, 98))$mean
