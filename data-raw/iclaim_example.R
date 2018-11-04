fb2a2e7051e9431f479d0883d6b928c1bdc29a3f
Edwin Ng <edwinng@uber.com>
2018-11-04 15:51:48 -0800

    moving data raw

---
+++ data-raw/iclaim_example.R
@@ -0,0 +1,51 @@
+library(Rlgt)
+library(gtrendsR)
+library(dplyr)
+library(lubridate)
+iclaim <- read.csv('./data-raw/ICNSA.csv', stringsAsFactors = FALSE)
+iclaim$DATE <- as.Date(iclaim$DATE)
+
+x <- gtrends(keyword = "unemployment", geo = "US")$interest_over_time
+
+search.unemploy1 <- gtrends(keyword = "unemployment", geo = "US", 
+                           time = "2010-01-01 2014-12-31")$interest_over_time
+search.unemploy2 <- gtrends(keyword = "unemployment", geo = "US", 
+                            time = "2015-01-01 2018-06-30")$interest_over_time
+search.unemploy <- rbind(search.unemploy1, search.unemploy2)
+search.filling1 <- gtrends(keyword = "filling", geo = "US", 
+                          time = "2010-01-01 2014-12-31")$interest_over_time
+search.filling2 <- gtrends(keyword = "filling", geo = "US", 
+                          time = "2015-01-01 2018-06-30")$interest_over_time
+search.filling <- rbind(search.filling1, search.filling2)
+search.job1 <- gtrends(keyword = "job", geo = "US", 
+                      time = "2010-01-01 2014-12-31")$interest_over_time
+search.job2 <- gtrends(keyword = "job", geo = "US", 
+                      time = "2015-01-01 2018-06-30")$interest_over_time
+search.job <- rbind(search.job1, search.job2)
+
+search.unemploy$date <- as.Date(search.unemploy$date)
+search.filling$date  <- as.Date(search.filling$date)
+search.job$date      <- as.Date(search.job$date)
+
+search.unemploy.clean <- search.unemploy %>%
+  dplyr::select(date, hits) %>%
+  rename(trend.unemploy = hits)
+search.filling.clean <- search.filling %>%
+  dplyr::select(date, hits) %>%
+  rename(trend.filling = hits)
+search.job.clean <- search.job %>%
+  dplyr::select(date, hits) %>%
+  rename(trend.job = hits)
+
+iclaims.example <- iclaim %>%
+  rename(claims = ICNSA, week = DATE) %>%
+  mutate(week = floor_date(week, unit = "weeks", week_start = 7) + 7,
+         claims = claims / 1000) %>%
+  inner_join(search.unemploy.clean, by = c('week' = 'date')) %>%
+  inner_join(search.filling.clean, by = c('week' = 'date')) %>%
+  inner_join(search.job.clean, by = c('week' = 'date')) 
+
+iclaims.example[,c('claims','trend.unemploy','trend.filling','trend.job')] <-
+  log(iclaims.example[,c('claims','trend.unemploy','trend.filling','trend.job')])
+
+save(iclaims.example, file = './data/iclaims.example.RData')
---
+++ data-raw/umcsent_example.R
@@ -0,0 +1,60 @@
+library(gtrendsR)
+library(dplyr)
+library(lubridate)
+umscent <- read.csv('./data-raw/UMCSENT.csv', stringsAsFactors = FALSE)
+umscent$DATE <- as.Date(umscent$DATE)
+
+# search engine
+search.engine <- gtrends(keyword = NA, geo = "US",
+                         category = 485, time = "all")$interest_over_time
+# financial planning
+financial.planning <- gtrends(keyword = NA, geo = "US",
+                              category = 903, time = "all")$interest_over_time
+# business news
+bus.news <- gtrends(keyword = NA, geo = "US",
+                    category = 784, time = "all")$interest_over_time
+# investing
+investing <- gtrends(keyword = NA, geo = "US",
+                     category = 107, time = "all")$interest_over_time
+# energy utilities
+energy.utilities <- gtrends(keyword = NA, geo = "US",
+                            category = 233, time = "all")$interest_over_time
+
+search.engine$date         <- as.Date(search.engine$date)
+financial.planning$date    <- as.Date(financial.planning$date)
+bus.news$date              <- as.Date(bus.news$date)
+investing$date             <- as.Date(investing$date)
+energy.utilities$date      <- as.Date(energy.utilities$date)
+
+search.engine <- search.engine %>%
+  dplyr::select(date, hits) %>%
+  rename(search.engine = hits)
+financial.planning <- financial.planning %>%
+  dplyr::select(date, hits) %>%
+  rename(financial.planning = hits)
+bus.news <- bus.news %>%
+  dplyr::select(date, hits) %>%
+  rename(bus.news = hits)
+investing <- investing %>%
+  dplyr::select(date, hits) %>%
+  rename(investing = hits)
+energy.utilities <- energy.utilities %>%
+  dplyr::select(date, hits) %>%
+  rename(energy.utilities = hits)
+
+umcsent.example.prestransform <- umscent %>%
+  rename(consumer.sent = UMCSENT, date = DATE) %>%
+  inner_join(search.engine, by = c('date')) %>%
+  inner_join(financial.planning, by = c('date')) %>%
+  inner_join(bus.news, by = c('date')) %>%
+  inner_join(investing, by = c('date')) %>%
+  inner_join(energy.utilities, by = c('date'))
+
+curr_series <- "consumer.sent"
+umcsent.example <- umcsent.example.prestransform %>%
+  mutate_at(.vars = c("consumer.sent", "search.engine",
+                      "financial.planning", "bus.news",
+                      "investing", "energy.utilities"),
+            .funs = log)
+
+save(umcsent.example, file = './data/umcsent.example.RData')
