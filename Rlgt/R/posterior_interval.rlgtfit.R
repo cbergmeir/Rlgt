a004170407fc94945f7e03c1698f168e6d2567d0
Christoph Bergmeir <christoph.bergmeir@monash.edu>
2019-01-23 13:57:52 +1100

    CRAN submission 0.1-1

--- Rlgt/R/posterior_interval.rlgtfit.R
+++ Rlgt/R/posterior_interval.rlgtfit.R
@@ -10,13 +10,13 @@
 #' @method posterior_interval rlgtfit
 #' @importFrom rstantools posterior_interval 
 #' @examples 
-#\dontrun{
+#'\dontrun{
 #' rlgt_model <- rlgt(lynx,
-#'      control=rlgt.control(MAX_NUM_OF_REPEATS=1, NUM_OF_ITER=2000, NUM_OF_CORES=1), verbose=TRUE)
+#'      control=rlgt.control(MAX_NUM_OF_REPEATS=1, NUM_OF_ITER=2000), verbose=TRUE)
 #'
 #' # print the model details
 #' posterior_interval(rlgt_model)
-#}
+#'}
 #' @export
 
 posterior_interval.rlgtfit <- function(object,
